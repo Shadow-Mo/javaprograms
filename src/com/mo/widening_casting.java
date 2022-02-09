f0d2cbfe7fa3c24f9b8e2f9e713a62dc49afcac9
Mounesha Zonde <mouneshe2002@gmail.com>
2022-02-09 23:37:05 +0530

    Widening casting(typecasting)

---
+++ src/com/mo/widening_casting.java
@@ -0,0 +1,10 @@
+package com.mo;
+
+public class widening_casting {
+    public static void main(String[] args) {
+        int intname = 9;
+        double doublename = intname;
+        System.out.println(intname);
+        System.out.println(doublename);
+    }
+}
