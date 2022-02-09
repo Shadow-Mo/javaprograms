8d3050827cefb271212c67a48d060e55a3413cfd
Mounesha Zonde <mouneshe2002@gmail.com>
2022-02-09 23:46:24 +0530

    narrowing casting (typecasting)

---
+++ src/com/mo/narrowing_casting.java
@@ -0,0 +1,10 @@
+package com.mo;
+
+public class narrowing_casting {
+    public static void main(String[] args) {
+        double doublename = 9.78d;
+        int intname = (int)doublename;
+        System.out.println(doublename);
+        System.out.println(intname);
+    }
+}
