ca216ade4a80e78f27c93fe2c18121a50e526d5f
Mounesha Zonde <mouneshe2002@gmail.com>
2022-02-10 09:45:37 +0530

    Operators

---
+++ src/com/mo/operators.java
@@ -0,0 +1,17 @@
+package com.mo;
+
+public class operators {
+    public static void main(String[] args) {
+        int x=5, y=5, z, p, q, r, s;
+        z= x+y;
+        p= x-y;
+        q= x*y;
+        r= x/y;
+        s= x%y;
+        System.out.println("Addition is " + z);
+        System.out.println("Subtraction is " + p);
+        System.out.println("Multiplication is " + q);
+        System.out.println("Division is " + r);
+        System.out.println("Modulus is " + s);
+    }
+}
