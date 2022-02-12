b1281921c18746342eeffabd5f9cf00133c774b5
Mounesha Zonde <mouneshe2002@gmail.com>
2022-02-12 13:05:53 +0530

    Booleans

---
+++ src/com/mo/booleans.java
@@ -0,0 +1,20 @@
+package com.mo;
+
+public class booleans {
+    public static void main(String[] args) {
+//        boolean values
+        boolean javafun = true;
+        boolean fishtasty = false;
+        System.out.println(javafun);    //outputs true
+        System.out.println(fishtasty);  //outputs false
+
+//        boolean expression
+        int x = 10;
+        int y = 9;
+        System.out.println(x > y);     //returns true
+
+        int x1 = 10;
+        System.out.println(x1==10);    //returns true
+        System.out.println(10==15);    //returns false
+    }
+}
