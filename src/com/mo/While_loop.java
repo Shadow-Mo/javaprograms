a1599a64e7ba08fc5493a1e571430a68c0b186f0
Mounesha Zonde <mouneshe2002@gmail.com>
2022-02-12 13:13:43 +0530

    While loop

---
+++ src/com/mo/While_loop.java
@@ -0,0 +1,19 @@
+package com.mo;
+
+public class While_loop {
+    public static void main(String[] args) {
+//        While loop
+        int i = 0;
+        while(i<5){
+            System.out.println(i);
+            i++;
+        }
+//        Do While loop
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        }
+        while(j<5);
+    }
+}
