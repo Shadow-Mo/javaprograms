db1c54de07b6bcc1d2e3c9bea119aa5acdaeb268
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2022-02-19 17:22:27 +0530

    Using Multiple Classes

---
+++ src/com/mo/first.java
@@ -0,0 +1,5 @@
+package com.mo;
+
+public class first {
+    int x=5;
+}
---
+++ src/com/mo/second.java
@@ -0,0 +1,8 @@
+package com.mo;
+
+public class second {
+    public static void main(String[] args){
+        first myobj = new first();
+        System.out.println(myobj.x);
+    }
+}
