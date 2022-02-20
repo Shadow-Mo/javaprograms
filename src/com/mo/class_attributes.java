08de9a8670d16b53bce5117e46faf418ea43dc79
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2022-02-20 11:03:46 +0530

    Class Attributes

---
+++ src/com/mo/class_attributes.java
@@ -0,0 +1,14 @@
+package com.mo;
+//We used the term "variable" for x in previous ex..
+// It is actually an ATTRIBUTE of the class
+
+public class class_attributes {
+//    Accessing Attributes
+    //create an object called "myobj" & print value of x
+    int x=5;
+    public static void main(String[] args){
+        class_attributes myobj = new class_attributes();
+        System.out.println(myobj.x);
+
+    }
+}
