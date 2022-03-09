4d9f07681f8cbf4cbdbf89e58ae375d6f7b3ff65
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2022-03-10 04:08:10 +0530

    Class Methods

---
+++ src/com/mo/class_methods.java
@@ -0,0 +1,11 @@
+package com.mo;
+//call mymeethod() inside main
+public class class_methods {
+    static void mymethod(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        mymethod();
+    }
+}
