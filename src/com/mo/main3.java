623817f0bd39bcd33fd789e41e6a1019820f1507
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2022-03-10 21:54:51 +0530

    Encapsulation
    The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users. 
    To achieve this, you must:
    --declare class variables/attributes as private
    --provide public get and set methods to access and update the value of a private variable

---
+++ src/com/mo/main3.java
@@ -0,0 +1,9 @@
+package com.mo;
+
+public class main3 {
+    public static void main(String[] args) {
+        person obj = new person();
+        obj.setName("John");
+        System.out.println(obj.getName());
+    }
+}
---
+++ src/com/mo/person.java
@@ -0,0 +1,13 @@
+package com.mo;
+
+public class person {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    //setter
+    public void setName(String newname){
+        this.name = newname;
+    }
+}
