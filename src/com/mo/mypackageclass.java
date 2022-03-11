d51db07cfe5c080379abdc1011ded83289fb34ad
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2022-03-11 05:33:54 +0530

    New Pack

---
+++ src/com/mo/mypackageclass.java
@@ -0,0 +1,7 @@
+package com.mo.mypack;
+//To create a package, use the package keyword:
+public class mypackageclass {
+    public static void main(String[] args) {
+        System.out.println("This is my package");
+    }
+}
