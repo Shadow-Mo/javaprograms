dae8c146d65611595fff9f8e76a0be7f8b90ecc1
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2022-03-25 21:09:37 +0530

    Date, Time and Format
    To display the current date, import the java.time.LocalDate class, and use its now() method.

---
+++ src/com/mo/date.java
@@ -0,0 +1,10 @@
+package com.mo.current;
+
+import java.time.LocalDate;
+
+public class date {
+    public static void main(String[] args) {
+        LocalDate myobj = LocalDate.now();
+        System.out.println(myobj);
+    }
+}
