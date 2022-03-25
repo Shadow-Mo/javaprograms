71a782e34cb589ff6dc15f005e43e14b653c5960
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2022-03-25 20:43:53 +0530

    User Input (Scanner)
    The Scanner class is used to get user input, and it is found in the java.util package.
    
    To use the Scanner class, create an object of the class and use any of the available methods found in the Scanner class documentation.

--- src/com/mo/scanner.java
+++ src/com/mo/scanner.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class scanner {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.println("Enter username");
-
-        String username = obj.nextLine();
-        System.out.println("Username is " + username);
+        Scanner myobj = new Scanner(System.in);
+        System.out.println("Enter name, age, salary: ");
+        String name = myobj.nextLine();
+        int age = myobj.nextInt();
+        int salary = myobj.nextInt();
+        System.out.println("Name is "+name +"\nAge is "+age +"\nSalary is "+salary);
     }
 }
