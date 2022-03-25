a7a52a7ef2bbd8e9a3f30d74df08d52bb410402f
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2022-03-25 21:10:45 +0530

    Date, Time and Format
    To display the current time (hour, minute, second, and nanoseconds), import the java.time.LocalTime class, and use its now() method.

---
+++ src/com/mo/time.java
@@ -0,0 +1,10 @@
+package com.mo.current;
+
+import java.time.LocalTime;
+
+public class time {
+    public static void main(String[] args) {
+        LocalTime myobj = LocalTime.now();
+        System.out.println(myobj);
+    }
+}
