2c039b77b36f91966ea3f3db57761282f5a7cb0d
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2022-09-02 18:43:27 +0530

    Add files via upload

---
+++ apnacollege/loops/while_loop.java
@@ -0,0 +1,15 @@
+package com.apnacollege.loops;
+
+public class while_loop {
+    public static void main(String[] args) {
+        int i=0;
+//        while (i<11){
+//            System.out.println(i);
+//            i++;
+//        }
+        do {
+            System.out.println(i);
+            i++;
+        }while (i<5);
+    }
+}
