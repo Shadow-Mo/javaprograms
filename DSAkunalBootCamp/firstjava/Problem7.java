c3c8474bf9b64db6505d7013bbb3910b33f09b7a
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2023-04-12 12:26:37 +0530

    WAP to calculate fibonacci number upto n numbers

---
+++ DSAkunalBootCamp/firstjava/Problem7.java
@@ -0,0 +1,16 @@
+package KunalDSABootCamp.firstjava;
+//to calculate fibonacci number upto n numbers
+public class Problem7 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b=1;
+        System.out.println(a+" "+b);
+        int count=10;
+        for (int i=2; i<count; i++){
+            int c= a+b;
+            System.out.println(" "+c);
+            a=b;
+            b=c;
+        }
+    }
+}
