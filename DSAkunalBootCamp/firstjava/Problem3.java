94c4d807bf10d16fe78ab8d6a334732145b1de1f
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2023-04-12 12:11:13 +0530

    Add files via upload

---
+++ DSAkunalBootCamp/firstjava/Problem3.java
@@ -0,0 +1,18 @@
+package com.KunalDSABootCamp.firstjava;
+
+import java.util.Scanner;
+
+//write a program to take input principal , time, rate(P ,T ,R) and find simple interest
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Principal = sc.nextInt();   //Rs.5000
+        int Rate = sc.nextInt();        //rate 10 pa
+        int Time = sc.nextInt();        //2 years
+        int SI = (Principal * Rate * Time)/100;
+        System.out.println("Principal is "+Principal);
+        System.out.println("Rate is "+Rate);
+        System.out.println("Time is "+Time);
+        System.out.println("Simple Interest is : "+SI);
+    }
+}
---
+++ DSAkunalBootCamp/firstjava/Problem5.java
@@ -0,0 +1,17 @@
+package com.KunalDSABootCamp.firstjava;
+
+import java.util.Scanner;
+
+//take 2 input numbers and print the largest number
+public class Problem5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a>b){
+            System.out.println("Largest number is "+a);
+        }else {
+            System.out.println("Largest number is "+b);
+        }
+    }
+}
