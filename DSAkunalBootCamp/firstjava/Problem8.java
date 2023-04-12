bb73f76c480a63baf5b8461c1889c59302965a66
Mounesha Zonde <91440078+Shadow-Mo@users.noreply.github.com>
2023-04-12 12:27:53 +0530

    WAP to find out whether given string is palindrome or not

---
+++ DSAkunalBootCamp/firstjava/Problem8.java
@@ -0,0 +1,29 @@
+package KunalDSABootCamp.firstjava;
+
+import java.util.Scanner;
+
+//to find out whether given string is palindrome or not
+public class Problem8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = sc.nextLine();
+        if (isPalindrome(str)){
+            System.out.println(str +" is a palindrome.");
+        }else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str){
+        int i=0, j=str.length()-1;
+        while (i<j){
+            if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
